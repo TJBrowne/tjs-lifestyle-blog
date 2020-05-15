5ee73ba5fcd14972b9018be800db207e17c57c02
Tiffany J Brown <tiffany.brownjc@gmail.com>
2020-05-15 15:11:50 -0400

    some more css on about me image

---
+++ index.js
@@ -0,0 +1,11 @@
+// (function() {
+//     var aboutMeImg = document.getElementById('fakeimg1').firstChild;
+//     img.onload = function() {
+//         if(img.height > img.width) {
+//             img.height = '100%';
+//             img.width = 'auto';
+//         }
+//     };
+
+//     aboutMeImg();
+
